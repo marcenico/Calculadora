05430540d72f28271d1b577bdb2bedc6c2c8855d
marcenp <marcenp23@gmail.com>
2018-12-01 15:51:47 -0300

    Creada clase con funciones de suma y resta

---
+++ Calculadora/src/calculadora/Operaciones/OpPrimaria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.Operaciones;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class OpPrimaria {
+    
+    public static float Suma(float a, float b){
+        return a + b;
+    }
+    
+    public static float Resta(float a, float b){
+        return a - b;
+    }
+}
