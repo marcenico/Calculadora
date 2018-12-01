d041ce57c6a741c6d2f564afd90c7db7e7747f68
Leonardo <44589066+Takaakira@users.noreply.github.com>
2018-12-01 15:48:57 -0300

    folder

---
+++ Calculadora/src/calculadora/Operaciones/OpSecundaria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.Operaciones;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class OpSecundaria {
+    
+    public static float Multiplicacion(float a, float b){
+        return a*b;
+    }
+    
+}
